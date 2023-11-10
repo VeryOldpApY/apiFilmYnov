9c060e351b888c557ceb8ed468b9dbee54f3e580
VeryOldpApY <VeryOldpApY@pm.me>
2023-11-10 14:20:07 +0100

    "Ajout de la prise en charge de la base de données SQLite pour la récupération de films
    Cette mise à jour introduit la connectivité de base de données SQLite à l'application Flask existante pour prendre en charge la récupération des données de films. Deux nouvelles routes "/film/list" et "/film/{id}" sont ajoutées pour récupérer la liste des films et un film par identifiant respectivement. Une nouvelle fonction d'aide "create_connection" établit une connexion avec la base de données SQLite. Le fichier .idea/sqldialects.xml a également été ajouté pour la prise en charge de SQLite dans le projet."

--- app.py
+++ app.py
@@ -1,12 +1,43 @@
+import sqlite3
+from sqlite3 import Error
 from flask import Flask
 
 app = Flask(__name__)
+bdd = None
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def create_connection(db_file):
+	""" create a database connection to the SQLite database
+		specified by the db_file
+	:param db_file: database file
+	:return: Connection object or None
+	"""
+	conn = None
+	try:
+		conn = sqlite3.connect(db_file)
+		print(sqlite3.version)
+	except Error as e:
+		print(e)
+	
+	return conn
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/film/list")
+def getListFilm():
+	cursor = bdd.cursor()
+	cursor.execute("SELECT * FROM film")
+	rows = cursor.fetchall()
+	return {rows}
+
+
+@app.route("/film/{id}")
+def getFilm():
+	cursor = bdd.cursor()
+	cursor.execute("SELECT * FROM film WHERE id = {id}")
+	rows = cursor.fetchall()
+	return {rows}
+
+
+if __name__ == "__main__":
+	bdd = create_connection(r"bdd.db")
+	app.run()
