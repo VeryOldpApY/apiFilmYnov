8b1d3df158efe774df9c3ce3767312cb3caea7eb
Very Old pApY <VeryOldpApY@pm.me>
2023-12-01 14:11:04 +0100

    Fix 4

--- test_app.py
+++ test_app.py
@@ -1,4 +1,4 @@
-import requests
+import app
 
 
 url = "http://localhost:5000"
@@ -6,12 +6,10 @@ headers = {'Content-Type': 'application/json'}
 
 
 def test_index():
-	payload = dict("")
-	assert requests.get(url+"/", data=payload, headers=headers).json() == {"status": "API is running"}
+	assert app.index() == {"status": "API is running"}
 
 
 def test_fixture():
-	payload = dict()
-	assert requests.get(url+"/fixture", data=payload, headers=headers).json() == {"status": "ok"}
+	assert app.setFixture() == {"status": "ok"}
 	
 	
